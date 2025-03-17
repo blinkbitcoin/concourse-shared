d4ce40a0c66a89fde1054b3fefac4ee1d7c59a2f
k9ert <kim@blinkbtc.com>
2025-03-17 19:07:45 +0100

    fix: some pnpm issues

--- shared/ci/tasks/cache-pnpm-deps.sh
+++ shared/ci/tasks/cache-pnpm-deps.sh
@@ -5,9 +5,26 @@ set -eu
 tar_out="$(pwd)/bundled-deps"
 
 pushd deps
-pnpm install
+# Install dependencies
+echo "  * pnpm install"
+pnpm install --shamefully-hoist
+
+# Get git reference for versioning
+echo "  * git log"
 git log --pretty=format:'%h' -n 1 > gitref
 
-tar -zcvf "${tar_out}/bundled-deps-v$(cat ../deps-version/number)-$(cat gitref).tgz" . > /dev/null
+# Create the output filename
+output_file="${tar_out}/bundled-deps-v$(cat ../deps-version/number)-$(cat gitref).tgz"
 
+# Use --dereference to convert hard links to regular files
+echo "  * tar ..."
+tar --dereference -zcf "$output_file" \
+    --exclude='.git' \
+    --exclude='.github' \
+    --exclude='ci' \
+    .
+
+echo "  * Contents:"
+tar -ztvf "$output_file"
+echo "  * Done!"
 popd
