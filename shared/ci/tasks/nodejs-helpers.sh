ad3cbefbb52f34f7ebc322bfe50a998087b6e373
k9ert <kim@blinkbtc.com>
2025-04-01 12:18:47 +0200

    fix: bumping for forked repos

--- shared/ci/tasks/nodejs-helpers.sh
+++ shared/ci/tasks/nodejs-helpers.sh
@@ -4,7 +4,7 @@
 #! Don't change this file, instead change it in github.com/blinkbitcoin/concourse-shared
 
 if [[ -z $(git config --global user.email) ]]; then
-  git config --global user.email "202112752+blinkbitcoinbot@users.noreply.github.com
+  git config --global user.email "202112752+blinkbitcoinbot@users.noreply.github.com"
 fi
 if [[ -z $(git config --global user.name) ]]; then
   git config --global user.name "CI blinkbitcoinbot"
