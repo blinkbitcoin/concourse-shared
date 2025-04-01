dafb65ba11f0b0594eafacad66828b3e76f0deb9
k9ert <kneunert@gmail.com>
2025-04-01 12:44:24 +0200

    fix: bumping for forked repos (and typo) (#6)
    Co-authored-by: k9ert <kim@blinkbtc.com>
    Co-authored-by: openoms <43343391+openoms@users.noreply.github.com>

--- ci/tasks/open-pr.sh
+++ ci/tasks/open-pr.sh
@@ -6,16 +6,24 @@ export GH_TOKEN="$(ghtoken generate -b "${GH_APP_PRIVATE_KEY}" -i "${GH_APP_ID}"
 
 pushd source-repo
 
+# For forked repos (many are forked from GaloyMoney) we need to explicitly specify the REPO_NAME below
+# So let's calculate here
+
+REPO_NAME=$(basename $(git config --get remote.origin.url) .git)
+REPO_OWNER=$(git config --get remote.origin.url | sed -n 's/.*github.com[:/]\([^/]*\).*/\1/p')
+FULL_REPO="${REPO_OWNER}/${REPO_NAME}"
+
 cat <<EOF >> ../body.md
 # Bump Shared Tasks
 
 This PR syncs in this repository, shared CI tasks from [concourse-shared](https://github.com/blinkbitcoin/concourse-shared).
 EOF
 
-gh pr close ${PR_BRANCH} || true
+gh pr close ${PR_BRANCH} --repo=${FULL_REPO} || true
 gh pr create \
   --title "ci(shared): bump vendored ci files" \
   --body-file ../body.md \
   --base ${BRANCH} \
   --head ${PR_BRANCH} \
-  --label blinkbitcoinbot
+  --label blinkbitcoinbot \
+  --repo=${FULL_REPO}
